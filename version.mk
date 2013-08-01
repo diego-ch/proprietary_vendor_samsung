c6bea6cd25acedb0cc88e3dbb5ef587c8f9c6675
Diego Porto <diego.cfporto@gmail.com>
2013-08-01 17:29:54 -0300

    update goo.im properties

--- version.mk
+++ version.mk
@@ -1,6 +1,6 @@
 # Goo IM Updater
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.board=$(PRODUCT_DEVICE) \
-    ro.goo.developerid=diegoch \
-    ro.goo.rom=cm_10 \
-    ro.goo.version=$(shell date +%s)
+	ro.goo.developerid=diegoch \
+	ro.goo.rom=cm_10 \
+	ro.goo.version=$(shell date +%s)
+
